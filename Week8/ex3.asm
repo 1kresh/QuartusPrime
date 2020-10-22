0a782e2f2f51bc172dd4f3093372e80021399bf4
Andrey Korokhov <andreykorokhov@yandex.ru>
2020-10-22 16:16:56 +0300

    week8

---
+++ Week8/ex3.asm
@@ -0,0 +1,18 @@
+.data
+fout: .asciiz "Answer : "
+e: .float 2.71828
+pi: .float 3.1415
+
+.text
+main:
+l.s $f1, e
+l.s $f2, pi
+mul.s $f3,$f1,$f1
+div.s $f12,$f3,$f2
+li $v0,4
+la $a0, fout
+syscall
+li $v0, 2
+syscall
+li $v0, 10
+syscall
